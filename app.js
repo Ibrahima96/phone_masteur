63e7565b74e14ea82bc02eaeb89972597832af81
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-04-05 00:37:58 +0000

    Add main application files including HTML structure, CSS styles, and JavaScript functionality for keyboard input and audio playback

---
+++ app.js
@@ -0,0 +1,40 @@
+let song = () => {
+  let audio = new Audio();
+  audio.src = "/assets/rings.mp3";
+  audio.play();
+};
+
+
+
+let btn = document.querySelectorAll("button");
+let input = document.getElementById("result");
+
+btn.forEach((item) => {
+  item.addEventListener("click", () => {
+    let valute = item.innerHTML.toLowerCase();
+
+    if (valute === "clear") {
+      input.value = ""; // Clear input
+    } else if (valute === "del") {
+      input.value = input.value.slice(0, -1); // Delete last character
+    } else if (valute === "") {
+      input.value += " "; // Add space
+    } else if (valute === "entre") {
+      alert("later......")
+    }
+    else if (valute === "maj") {
+      input.value = input.value.toUpperCase(); // Convert to uppercase
+      
+    }
+    
+    else {
+      input.value += valute; // Add character
+    }
+
+    song();
+    confetti();
+  });
+});
+
+
+
